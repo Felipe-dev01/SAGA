9c1eb9be8e529ab464aca239001730170538536f
Felipe Farias <161083885+Felipe-dev01@users.noreply.github.com>
2024-08-05 21:27:23 -0300

    add user responsável

--- src/SCRIPT/scripts.js
+++ src/SCRIPT/scripts.js
@@ -2,7 +2,7 @@ function updateRM() {
     let roleSelect = document.getElementById("role");
     let rmLabel = document.getElementById("rm-label");
 
-    if (roleSelect.value === "professor") {
+    if (roleSelect.value === "professor" || "responsavel") {
         rmLabel.textContent = "Login:";
     } else {
         rmLabel.textContent = "RM:";
@@ -13,7 +13,7 @@ function updateCPF() {
     let roleSelect = document.getElementById("role");
     let cpflabel= document.getElementById("cpf-label");
 
-    if (roleSelect.value === "professor") {
+    if (roleSelect.value === "professor" || "responsavel") {
         cpflabel.textContent = "CPF:";
     } else {
         cpflabel.textContent = "CPF do aluno:";
