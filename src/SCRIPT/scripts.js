4932a39317d0c6520a0f237d802879d4329bd845
Felipe Farias <161083885+Felipe-dev01@users.noreply.github.com>
2024-08-09 20:54:01 -0300

    Update scripts.js

--- src/SCRIPT/scripts.js
+++ src/SCRIPT/scripts.js
@@ -37,7 +37,38 @@ document.getElementById("role").addEventListener("change", function() {
     updateCPF();
 });
 
+
+
 // Chama as funções updateRM e updateCPF para configurar os labels corretamente
 // quando a página é carregada pela primeira vez
 updateRM();
 updateCPF();
+
+if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+        console.log('Service Worker registrado com sucesso:', registration);
+      }).catch(function(error) {
+        console.log('Falha ao registrar o Service Worker:', error);
+      });
+    }
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('my-pwa-cache').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/../../index.html',
+        '/../CSS/styles.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
